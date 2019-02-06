26fbc04b30f03f5c3b210f9b0ff1f1403489cbc9
Kim Borgen <me@kimborgen.com>
2019-02-06 14:00:48 +0100

    Add skeleton code and filestructure

---
+++ software/src/main.py
@@ -0,0 +1,31 @@
+# This file represents the main file and entrypoint to this entire project
+#   This project is a part of the EiT village NTNU Cyborg
+#   In this project we will demonstrate an exepriment with the neurocellculture.
+#   This is done by connecting a hardware platform and a software platform to the MEAME
+#   interface at St. Olavs.
+#
+#   Github repo: https://github.com/hjerner-i-team/CREPE
+
+# Function that starts/setups the required components in this project
+def setup(): 
+    pass
+    
+# Function that starts the main loop or the processes that is required to run this loop 
+def start():
+    pass
+
+# Function that runs the required shutdown commands before the project is closed 
+def shutdown():
+    pass
+
+# Main entry point to entire project
+def main():
+    #Welcome to our main function! Hope you have a happy time
+    setup()
+
+    start()
+    
+    shutdown()
+
+if __name__ == "__main__":
+    main()
