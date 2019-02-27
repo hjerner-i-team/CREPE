e12893b9d994be92cc26ca2e667b270fc0f82014
Kim Borgen <me@kimborgen.com>
2019-02-27 12:26:58 +0100

    Add subproccess spawning

--- software/src/neuro_processing/primary_dataloops/start.py
+++ software/src/neuro_processing/primary_dataloops/start.py
@@ -1,13 +1,19 @@
+""" Import fix - check README for documentation """ 
+import os,sys,inspect 
+__currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+sys.path.insert(0, __currentdir[0:__currentdir.find("software/src")+len("software/src")])
+""" End import fix """
+
 # This file will keep track of which start the primary dataloop.
-from .modus import PrimaryDataLoopModus, PrimaryDataLoopModusEnum
-import primary_dataloops.example as PDexample
+
+from neuro_processing.primary_dataloops.modus import PrimaryDataLoopModus, PrimaryDataLoopModusEnum
+from neuro_processing.primary_dataloops.implementations.example import start as example
 
 modus = PrimaryDataLoopModus()
 
 def main():
-
     print(modus)
-    PDexample.main()
+    example.main()
 
 if __name__ == "__main__":
     main()
