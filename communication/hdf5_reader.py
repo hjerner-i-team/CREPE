15015e05182dca27b41e373c649a940fef87ebb6
Kim Borgen <me@kimborgen.com>
2019-04-02 19:44:44 +0200

    Change hdf5_reader from streamservice to queueservice

--- communication/hdf5_reader.py
+++ communication/hdf5_reader.py
@@ -4,22 +4,25 @@ __currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentfr
 sys.path.insert(0, __currentdir[0:__currentdir.find("/CREPE")+len("/CREPE")])
 """ End import fix """
 
-from communication.stream_service import StreamService, DataModus
+from communication.queue_service import QueueService, start_and_run_queue_service 
 from settings import DEFAULT_STREAM_DIMENSION
 import time
 import random
 
 import h5py
 import numpy as np
+from enum import Enum
+class HDF5Mode(Enum):
+    H5 = 0
+    TEST = 1
 
-# The path to the test_data folder. sys.path[0] is the src folder
-path_to_test_data = sys.path[0][0:-1 * len("/CREPE")] + "/test_data/"
+# reads from a hdf5 file and puts it unto the queue, or generates a random test stream and outputs it to the queue
+class HDF5Reader(QueueService):
 
-class HDF5Reader(StreamService):
-
-    def __init__(self, name, port, file_path="4.h5"):
-        StreamService.__init__(self, name, port, DataModus.DATA)
+    def __init__(self, queue_out=None, file_path=None, mode=HDF5Mode.TEST):
+        QueueService.__init__(self, "HDF5READER", queue_out=queue_out)
         self.file_path = file_path
+        self.mode = mode
 
     # Generates a 2d numpy array from a .h5 file to self.stream
     def generate_H5_stream(self): 
@@ -30,7 +33,11 @@ class HDF5Reader(StreamService):
         # Use the program hdfviewer or check our upcomming documentation for full .h5 format
         data = f['Data']['Recording_0']['AnalogStream']['Stream_0']['ChannelData']
         # this will return a h5py object so we convert it to a list
-        self.append_stream_segment_data(list(data))
+        print("[CREPE.communication.hdf5_reader] len of a row: ", len(data[0]))
+        for i in range(100, len(data[0]) + 1, 100):
+            self.put(data[:, i - 100:i])
+        remaining = data[:, len(data) - i % 100:len(data)]
+        self.put(remaining)
         print("[CREPE.communication.hdf5_reader] data pushed to stream")
 
     # Generates a 2d matrice with random numbers to self.stream for testing purposes
@@ -48,26 +55,29 @@ class HDF5Reader(StreamService):
     
     def _generate_random_test_segment_list(self, _range):
         rand_data = [[random.random() * 200 for i in range(_range)] for j in range(DEFAULT_STREAM_DIMENSION)]
-        print(rand_data)
         return rand_data
-        
 
-    def start(self):
-        #print("DebugPre ", self)
-        self.start_loop(self.loop)
-
-    def loop(self):
-        while True:
-            seg = self._generate_random_test_segment_list(100)
-            self.append_stream_segment_data(seg)
-            # sleep for 0.5 seconds
-            #time.sleep(0.5)
-    
-
-
-
+    def run(self):
+        if self.mode == HDF5Mode.H5:
+            self.generate_H5_stream()
+        elif self.mode == HDF5Mode.TEST:
+            i = 0
+            while True:
+                seg = self._generate_random_test_segment_list(100)
+                self.put(seg)
+                i += len(seg[0])
+                print(i)
+                # sleep for 0.5 seconds
+                time.sleep(0.005)
+            
 
 if __name__ == "__main__":
-    h = HDF5Reader()
-    h.generate_H5_stream()
-    print(h.stream)
+    #h = HDF5Reader()
+    # h.generate_H5_stream()
+    hdf5_process, hdf5_out = start_and_run_queue_service(HDF5Reader, 
+            file_path="../test_data/4.h5", mode=HDF5Mode.TEST)
+    i = 0
+    while True:
+        i += len(hdf5_out.get()[0])
+        print(i)
+
