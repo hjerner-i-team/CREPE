730e9b52973d8b6bb967a821dec883a7e996b6b4
lastCent <richard.bman@gmail.com>
2019-04-24 11:04:03 +0200

    updated api test

--- communication/hw_api/api-tests.py
+++ communication/hw_api/api-tests.py
@@ -1,26 +1,37 @@
 import requests, json
+from CREPE.communication.meame_speaker.config_decimal import *
+from CREPE.communication.meame_speaker.speaker import *
 
 def input_test():
     test_mat = [
-        [1, 2, 3, 4, 5],
-        [6, 7, 8, 9, 10],
-        [11, 12, 13, 14, 15],
-        [16, 17, 18, 19, 20]
+        [20, 20, 20, 20, 20, 20, 20, 20],
+        [20, 20, 20, 26, 26, 20, 20, 20],
+        [20, 20, 26, 26, 26, 26, 20, 20],
+        [20, 26, 26, 26, 26, 26, 26, 20],
+        [20, 26, 26, 26, 26, 26, 26, 20],
+        [20, 20, 26, 26, 26, 26, 20, 20],
+        [20, 20, 20, 26, 26, 20, 20, 20],
+        [20, 20, 20, 20, 20, 20, 20, 20],
+        [20, 20, 20, 20, 20, 20, 20, 20], 
     ] 
     # r = requests.get('http://127.0.0.1:5000/hw-api/input/')
     json_mat = json.dumps(test_mat)
     print(json_mat)
-    r = requests.post('http://127.0.0.1:5000/hw-api/input/', data=json_mat)
+    r = requests.post('http://129.241.209.138:5000/hw-api/input/', data=json_mat)
     print(r.status_code)
 
 def output_test():
-    r = requests.get('http://127.0.0.1:5000/hw-api/output/')
+    r = requests.get('http://129.241.209.138:5000/hw-api/output/')
     print(r.status_code)
     print(r.text)
+
+def apply_stim(freq):
+    set_stim(0, freq)
 
 def main():
     input_test()
     output_test()
+    apply_stim(5000)
 
 if __name__ == '__main__':
     main()
